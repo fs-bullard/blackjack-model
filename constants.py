cf06ebdc3e6fed00ce2547f6cb06a025e51ff88f
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-10-02 17:29:21 +0100

    using full names

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@
 Contains constants needed for blackjack game
 '''
 
-SUITS = ['C', 'S', 'H', 'D']
+SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
 
-RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+RANKS = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
